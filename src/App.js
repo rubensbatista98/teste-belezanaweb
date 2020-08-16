97031394445b674a1321630b434d19c9995c79d2
Rubens Batista <batistarubinho@gmail.com>
2020-08-16 16:30:32 -0300

    Feita a estrutura do Cabecalho

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import Header from "./components/Header";
+
 import Routes from "./routes";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
+
         <Suspense fallback="Carregando...">
           <Routes />
         </Suspense>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { useRouteMatch, withRouter } from "react-router-dom";
+import { useRouteMatch } from "react-router-dom";
 
 const Header = () => {
-  console.log(useRouteMatch());
-  return <h1>hhhww</h1>;
+  const route = useRouteMatch();
+
+  return (
+    <header>
+      <strong active={route.path === "carrinho"}>Sacola</strong>
+      <strong active={route.path === "pagamento"}>Pagamento</strong>
+      <strong active={route.path === "sucesso"}>Confirmação</strong>
+    </header>
+  );
 };
 
-export default withRouter(Header);
+export default Header;

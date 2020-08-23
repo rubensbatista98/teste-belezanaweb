01175651a3c0bf4d88c57a65d63943e7cb860c46
Rubens Batista <batistarubinho@gmail.com>
2020-08-22 23:27:06 -0300

    Feita a estrutura da pagina de pagamento

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,62 +1,51 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import SectionPanel from "../../components/SectionPanel";
+import PurchaseSummary from "../../components/PurchaseSummary";
+import Container from "../../ui/Container";
+import Button from "../../ui/Button";
+
+import * as S from "./styles";
 
 const Payment = () => {
   return (
-    <div>
-      <section>
-        <h1>Cartão de Credtio</h1>
+    <Container>
+      <SectionPanel title="Cartão de Credito">
+        <form>
+          <section>
+            <label htmlFor="credit_card">Número do cartão:</label>
+            <input
+              type="text"
+              id="credit_card"
+              placeholder="____.____.____.____"
+            />
+          </section>
 
-        <div>
-          <form>
+          <section>
+            <label htmlFor="name">Nome do Titular:</label>
+            <input type="text" id="name" placeholder="Como no cartão" />
+          </section>
+
+          <div>
             <section>
-              <label htmlFor="credit_card">Número do cartão:</label>
-              <input
-                type="text"
-                id="credit_card"
-                placeholder="____.____.____.____"
-              />
+              <label htmlFor="validate">Validade (mês/ano):</label>
+              <input type="text" id="validate" placeholder="__/____" />
             </section>
 
             <section>
-              <label htmlFor="name">Nome do Titular:</label>
-              <input type="text" id="name" placeholder="Como no cartão" />
+              <label htmlFor="cvv">CVV:</label>
+              <input type="text" id="cvv" placeholder="___" />
             </section>
+          </div>
+        </form>
+      </SectionPanel>
 
-            <div>
-              <section>
-                <label htmlFor="validate">Validade (mês/ano):</label>
-                <input type="text" id="validate" placeholder="__/____" />
-              </section>
+      <S.Wrapper>
+        <PurchaseSummary />
 
-              <section>
-                <label htmlFor="cvv">CVV:</label>
-                <input type="text" id="cvv" placeholder="___" />
-              </section>
-            </div>
-          </form>
-        </div>
-      </section>
-
-      <section>
-        <div>
-          <span>Produtos</span>
-          <span>R$ 624,80</span>
-        </div>
-
-        <div>
-          <span>Frete</span>
-          <span>R$ 5,30</span>
-        </div>
-
-        <div>
-          <strong>Total</strong>
-          <strong>R$ 600,10</strong>
-        </div>
-      </section>
-
-      <Link to="/sucesso" children="Finalizar o pedido" />
-    </div>
+        <Button to="/sucesso" children="Finalizar o pedido" />
+      </S.Wrapper>
+    </Container>
   );
 };
 
